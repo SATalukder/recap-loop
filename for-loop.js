ea50522ef6e6a08ef5a67a91a2d964d679f0a4d7
SATalukder <shajibahmad152@gmail.com>
2023-01-06 16:32:53 +0600

    for-loop

---
+++ for-loop.js
@@ -0,0 +1,18 @@
+
+
+// for (var roastGiven = 1; roastGiven <= 7; roastGiven++) {
+//     console.log(roastGiven);
+// }
+
+// for (var i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+//odd number
+
+// for (var i = 1; i <= 20; i += 2) {
+//     console.log(i);
+// }
+//even number
+for (var i = 0; i <= 20; i += 2) {
+    console.log(i);
+}
