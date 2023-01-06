b8fdd9f1c7e46c087437b448a81c6af27dd781ba
SATalukder <shajibahmad152@gmail.com>
2023-01-07 00:31:29 +0600

    practice

--- array-loop.js
+++ array-loop.js
@@ -1,5 +1,14 @@
-var numbers = [45, 86, 34, 87, 12, 98, 82];
+// var numbers = [45, 86, 34, 87, 12, 98, 82];
 
-for (var i = 0; i < numbers.length; i++) {
-    console.log(numbers[i]);
+// for (var i = 0; i < numbers.length; i++) {
+
+//     var element = numbers[i];
+//     console.log(element);
+// }
+
+var items = ['bottle', 'mobile', 'charger', 'watch', 'book', 'paper'];
+
+for (var i = 0; i < items.length; i++) {
+
+    console.log(items[i]);
 }
---
+++ practice-chalenge.js
@@ -0,0 +1,11 @@
+var practiceChalenge = ['1. At 8 pm module unlock. ', '2. See Video and practice.', '3. looking video and take notes', '4. Ending module pactice myself', '5. Join support seccion.'];
+
+for (var i = 0; i < 2 * practiceChalenge.length; i++) {
+    console.log(i);
+    console.log('1. At 8 pm module unlock. ');
+    console.log('2. See Video and practice.');
+    console.log('3. looking video and take notes');
+    console.log('4. Ending module pactice myself');
+    console.log('5. Join support seccion.');
+
+}
