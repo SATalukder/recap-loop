5badcc6b5bb93cda69d2d08ef61e6cdd9e9da42f
SATalukder <shajibahmad152@gmail.com>
2023-01-06 16:13:57 +0600

    use while loop and calculate odd and even

---
+++ while-loop-example.js
@@ -0,0 +1,20 @@
+var number = 1; //loop variable
+
+// while (number <= 10) {
+//     console.log(number);
+//     number++;
+// }
+//odd numbers
+while (number <= 20) {
+    console.log(number);
+    number += 2;
+}
+
+
+//even numbers
+// while (number <= 20) {
+//     if (number % 2 == 0) {
+//         console.log(number)
+//     }
+//     number++;
+// }
